8c1c59b09953f2e900caf980384475398a7cddb9
Henri <ecch9210@gmail.com>
2021-06-11 21:22:27 -0500

    Category-route finished

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,10 +48,47 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  // Exercise 13 ORM
+  // Updates book based on its isbn
+//   // Calls the update method on the Book model
+    Category.update(
+// All the fields you can update and the data attached to the request body.
+{
+id: req.body.id,
+category_name: req.body.category_name
+}
+
+    )
+.then((updatedCategory)=>{
+res.json(updatedCategory);
+})
+.catch((err) => res.json(err))
+//     .then((updatedBook) => {
+//       // Sends the updated book as a json response
+//       res.json(updatedBook);
+//     })
+//     .catch((err) => res.json(err));
 });
+  
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
