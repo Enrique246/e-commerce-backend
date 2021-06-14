ecc137677da9afaf85a82d2ef37819957bed2b8e
Henri <ecch9210@gmail.com>
2021-06-13 22:00:49 -0500

    Product routes working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -18,7 +18,7 @@ router.get('/',async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   try {
     const productData = await Product.findByPk(req.params.id, {
@@ -39,6 +39,20 @@ router.get('/:id', (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
+  Product.create(
+    {
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: req.body.tagIds
+    })
+  // {
+  //   product_name:"Football",
+  //   price: 400.00,
+  //   stock:5,
+  //   tagIds: [1,2,3,4,5]
+  // })
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -111,8 +125,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
